c9a0e470025d480fd0e8e5fb8bdb9c8d68dfd9ce
Aaron Buxbaum <aaronbuxbaum@gmail.com>
2026-07-27 22:28:26 -0400

    add route to sentry

--- next.config.ts
+++ next.config.ts
@@ -27,15 +27,37 @@ const nextConfig: NextConfig = {
 };
 
 export default withSentryConfig(nextConfig, {
-  // Forwards recovery and profiling data
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+  // TODO: remove to replace with environment variable
+  org: "diveday",
+
+  // TODO: remove to replace with environment variable
+  project: "diveday",
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Tunnel Sentry requests through this route to circumvent ad blockers
-  tunnelRoute: "/tunnel",
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/monitoring",
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
   webpack: {
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+
+    // Tree-shaking options for reducing bundle size
     treeshake: {
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
       removeDebugLogging: true,
     },
   },
